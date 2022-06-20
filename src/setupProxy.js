f40f744dae66d19faa6661c78cd9ab2d9c08e42c
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-20 14:44:47 +1000

    front startup

---
+++ src/setupProxy.js
@@ -0,0 +1,8 @@
+const { createProxyMiddleware:proxy } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use('/api',proxy({
+    target:"http://localhost:3030",
+    changeOrigin:true
+  }))
+}
