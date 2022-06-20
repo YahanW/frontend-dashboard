cee7b79585961281a321cb9dfa2f8931fc15bc30
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-20 15:44:41 +1000

    redux thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+
+const store=createStore(()=>{},applyMiddleware())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <div>it works!</div>
+  <Provider store={store}>
+    <div>it works!</div>
+  </Provider>
+ 
 );
