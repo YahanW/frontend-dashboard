7e21809daf9906a6f8bd7ebc116f01e64378a1f5
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-20 15:14:51 +1000

    proxy configuration changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware:proxy } = require('http-proxy-middleware');
+const { createProxyMiddleware:proxy } = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use('/api',proxy({
-    target:"http://localhost:3030",
+    target:"http://localhost:3033",
     changeOrigin:true
   }))
 }
