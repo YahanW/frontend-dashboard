0202c1f6c4f1dc7b12ca753528c163971f1f70d8
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-20 15:57:44 +1000

    composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store=createStore(()=>{},applyMiddleware())
+const store=createStore(()=>{},composeWithDevTools(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
