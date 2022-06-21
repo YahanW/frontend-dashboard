4afae235f4990cd8312e7b24f10197ff5b9bf5a9
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-21 16:04:00 +1000

    file change

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Main from "./pages/Main";
 import LoginForm from "./pages/LoginForm"
 import Registration from "./pages/Registration"
+import Baizhan from "./pages/Baizhan"
 
 export default(
     <Router>
         <Routes>
-            <Route path='/' element={<Main/>}>
-                <Route index element={<h1>default page</h1>}/>  
-            </Route>
+            <Route path='/' element={<Baizhan/>}/>
             <Route path='/login' element={<LoginForm/>}></Route>
             <Route path='/register' element={<Registration/>}></Route>
         </Routes>
