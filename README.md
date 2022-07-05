0ed6862893572829e7c23bf8a1d2b9a74fd11454
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-05 11:38:07 +1000

    configure port 3030 backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 1.proxy configuration: https://create-react-app.dev/docs/proxying-api-requests-in-development/
+
+2. when tarn start front-end Proxy created with a specific port. if change port proxy at backend, then refresh frontend
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware:proxy } = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use('/api',proxy({
-    target:"http://localhost:3033",
+    target:"http://localhost:3030",
     changeOrigin:true
   }))
 }
