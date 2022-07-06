68a506513d05a624c228e159cc51644f7ce04ef4
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-06 14:50:15 +1000

    setFieldsValue use Form name not props

--- src/pages/Services/ModalForm.js
+++ src/pages/Services/ModalForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Modal,Form,Input,Select,InputNumber,Dropdown,Radio,Space,Pagination} from 'antd'
-import {Icons} from '../../commons'
+import { Icons } from '../../commons'
 import _ from 'lodash'
 
 class ModalForm extends Component {
@@ -8,20 +8,26 @@ class ModalForm extends Component {
 constructor(props){
     super(props)
     this.state={allIcons:Icons,currentIcons:Icons.slice(0,10)}
+    //initial display 10 icons as default
 }
-
 formRef=React.createRef()
 
-onCancel=()=>{
-    //close modal
+onCancel=()=>{ //user clicks on the cancel button, close modal
     this.props.dispatch({ //passing state to modal through dispatch
         type:'hide'
     })
-    
+}
+onSave=(values)=>{
+    console.log(values)
+}
+layout={ 
+    //form layout
+    labelCol:{span:6},
+    wrapperCol:{span:20}
 }
 
 onTyping = (e) =>{
-    let {value} = e.target
+    let {value} = e.target  //event was dispatched
     value=_.trim(value) //remove space
     const tempIcons=[] //store the matched Icons
 
@@ -32,36 +38,31 @@ onTyping = (e) =>{
                 tempIcons.push(item) //push to temp
             }
         })
-        this.setState({icon:value,allIcons:tempIcons,currentIcons:tempIcons.slice(0,10)})
+        this.setState({icons:value,allIcons:tempIcons,currentIcons:tempIcons.slice(0,10)})
         //update displayed Icons
         return
     }
-    this.setState({icon:value,allIcons:Icons,currentIcons:Icons.slice(0,10)})
-}
-onSave=(values)=>{
-    console.log(values)
+    this.setState({icons:value,allIcons:Icons,currentIcons:Icons.slice(0,10)})
 }
 
 onSelected=(e)=>{
-    //get select Icons value
-    const {value}=e.target
-    this.setState({icon:value})
+    const {value}=e.target   //get select Icons value
+    //console.log(e.target)
+    this.setState({icons:value}) //passing icons icon value
     //manually set input
-    this.formRef.current.setFieldsValue({icon:value})
+    this.formRef.current.setFieldsValue({icons:value})
     
 }
 
 
-layout={
-    labelCol:{span:6},
-    wrapperCol:{span:20}
-}
-  render() {
+
+render() {
     return (
     <Modal visible width={600} title={this.props.title}
         onCancel={this.onCancel}
         onOk={()=>this.formRef.current.submit()}
     >
+    
         <Form {...this.layout} ref={this.formRef} onFinish={this.onSave}>
             <Form.Item label="Access Level">
                 {'Merchant'}
@@ -70,40 +71,6 @@ layout={
                 rules={[{required:true, message: 'Please input your Service Name!'}]}>
                 <Input/>
             </Form.Item>
-
-            <Form.Item label="Icon" name='Icons' rules={[{required:true,message: 'Please select your Icon!'}]}>
-               <Dropdown trigger={['click']} 
-                overlayStyle={{background:'#fff',padding:10}}
-                overlay={<>
-                    <Radio.Group onChange={this.onSelected}>
-                        <Space direction='vertical'>
-                            {this.state.currentIcons.map(item=>{
-                                return <Radio value={item.name}>
-                                    {React.createElement(item.renderDf)}
-                                    <span style={{margin:5}}>{item.name}</span>
-                                </Radio>
-                            })}
-                        </Space>
-
-                    </Radio.Group>
-                    <div style={{textAlign:'right',padding:10}}>
-                        <Pagination showSizeChanger={false} size="small" 
-                        total={this.state.allIcons.length}
-                        onChange={(page,pageSize)=>this.setState({
-                            currentIcons:this.state.allIcons.slice(pageSize*(page-1),pageSize*page)
-                        })}
-                    />
-                </div>
-                </>
-                }> {/*The end of dropdown*/}
-                    <Input prefix={React.createElement(
-                        (_.find(this.state.allIcons,(item)=>
-                            item.name==this.state.Icons)||{}).renderDf||'span'
-                        )} onChange={this.onTyping} value={this.state.icon}/>
-
-               </Dropdown>
-            </Form.Item>
-
             <Form.Item label="Merchant Name" name='mname' 
                 rules={[{required:true,message: 'Please input your Merchant Name!'}]}>
                 <Input/>
@@ -119,7 +86,46 @@ layout={
             <Form.Item label="Capacity 1-100" name='capacity' rules={[{required:true}]}>
                 <InputNumber min={1} max={100} defaultValue={0}/>
             </Form.Item>
+            {/**Icons */}
+
+            <Form.Item label='Icons' name='icons' rules={[{required:true,message: 'Please select your Icon!'}]}> 
+                <Dropdown 
+                    trigger={['click']} 
+                    overlayStyle={{background:'#fff',padding:10}}
+                    overlay={ 
+                    <> 
+                        <Radio.Group onChange={this.onSelected}>
+                            <Space direction='vertical'>
+                            {this.state.currentIcons.map(item=>{
+                                return <Radio value={item.name}>
+                                    {React.createElement(item.renderDf)}    
+                                    <span style={{margin:5}}>{item.name}</span>
+                                </Radio>
+                            })}
+                            </Space>
+                        </Radio.Group>
+
+                        <div style={{textAlign:'right',padding:10}}>
+                            <Pagination showSizeChanger={false} size="small" total={this.state.allIcons.length}
+                            onChange={(page,pageSize)=>this.setState({
+                                currentIcons:this.state.allIcons.slice(pageSize*(page-1),pageSize*page)})}
+                            />
+                        </div>
+
+                    </>}
+  /**Dropdown*/> 
+                    <Input prefix={
+                        React.createElement((_.find(this.state.allIcons,(item)=>
+                            item.name==this.state.icons)||{}).renderDf||'something')} 
+                        onChange={this.onTyping} 
+                        value={this.state.icons}
+                      />
+                </Dropdown>
+            </Form.Item>
+
         </Form>
+
+    
     </Modal>
     )
   }
