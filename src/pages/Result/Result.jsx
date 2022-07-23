193222f9110889307baa67d23ed2ea18b2c7f148
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-23 18:42:46 +1000

    filters half way

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Header from '../../layout/Header'
 import './Result.css'
+import {useState} from 'react'
 
 function Result() {
-  
+  const serType = [ "Venues","Hosts","Decoration","Performance","Security","Rental","Venues","Hosts","Decoration","Performance","Security",];
+  const areType = [ "Hobart","SandyBay","Kinston","NewTown","South Hobart"];
+  const bgtType = [ "$50-$99","$100-$499","$500-$999","$1000+"];
+  const [isActiveRight, setIsActive] = useState(false);
+  const [isActiveLeft, setLeft] = useState(false);
+  var areaID = 1;
+  const handleSelect = () =>{
+    setIsActive(!isActiveRight)
+  }
     return (
       <div className='result'>
         <Header/> {/**Navbar */}
@@ -16,18 +25,69 @@ function Result() {
         </div>
 
         <div className='filter'> 
+
           <div className='selection'>
-            <p>Service Type</p>
+            <div className='left'>
+              <p>Service Type</p>
+              <p className='all'>All</p>
+            </div>
+
+           <div className='right right-service'>
+            <ul>
+              {
+                serType.map((ele,index)=>{
+                  return <li key={index}
+                  style={{backgroundColor:isActiveRight?'darkseagreen':'',color:isActiveRight?'white':'black'}}
+                  
+                  onClick={handleSelect}>{ele}</li>
+                })
+              }
+              </ul>
+              
+           </div>
+           
           </div>
+          <hr className='hrSearch'/>
           <div className='selection'>
-            <p>Area</p>
+            <div className='left'>
+              <p>Area</p>
+              <p className='all'>All</p>
+            </div>
+            <div className='right right-area'>
+            <ul>
+              {
+                areType.map((ele,index)=>{
+                  return <li key={index} 
+                  style={{backgroundColor:isActiveRight?'darkseagreen':'',color:isActiveRight?'white':'black'}}
+                  onClick={handleSelect}
+                         >{ele}</li>
+                })
+              }
+              </ul>
+           </div>
           </div>
+          <hr className='hrSearch'/>
           <div className='selection'>
-            <p>Budget</p>
+            <div className='left'>
+                <p>Budget</p>
+                <p className='all'>All</p>
+            </div>
+            <div className='right right-budget'>
+              <ul>
+                {
+                  bgtType.map((ele,index)=>{
+                    return <li key={index}
+                    style={{backgroundColor:isActiveRight?'darkseagreen':'',color:isActiveRight?'white':'black'}}
+                  onClick={handleSelect}>{ele}</li>
+                  })
+                }
+                </ul>
+            </div>
           </div>
-        </div>
-        <div className='itemList'>
-          <div className='item'></div>
+          <hr className='hrSearch'/>
+          <div className='itemList'>
+            <div className='item'></div>
+          </div>
         </div>
       </div>
     )
