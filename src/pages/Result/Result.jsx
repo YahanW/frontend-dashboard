81d16270af38923d3534335d55f6f07007058efc
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-07-23 15:25:19 +1000

    search bar

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Header from '../../layout/Header'
+import './Result.css'
 
 function Result() {
+  
     return (
-      <div>
-        <Header/>
-        <h2>
-        Searching Result
-        </h2>
+      <div className='result'>
+        <Header/> {/**Navbar */}
+        <div className='find'>
+          <div className='logo'></div>
+            <div className='formSearch'>
+              <form><input placeholder='Searching by Merchant or Service'/></form>
+              <div className='iconSearch'></div>
+            </div>
+        </div>
+
+        <div className='filter'> 
+          <div className='selection'>
+            <p>Service Type</p>
+          </div>
+          <div className='selection'>
+            <p>Area</p>
+          </div>
+          <div className='selection'>
+            <p>Budget</p>
+          </div>
+        </div>
+        <div className='itemList'>
+          <div className='item'></div>
+        </div>
       </div>
     )
 }
