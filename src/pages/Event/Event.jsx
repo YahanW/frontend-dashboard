a705f45dfb07e3f71862ce1707aee7625d18e996
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-08-10 22:01:22 +1000

    building more

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -3,62 +3,68 @@ import Header from "../../layout/Header";
 import './Event.css';
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import Footer from '../Home/homes/Footer';
 export default function Event(){
 
-const [events,setEvents] = useState({list:[]})
+const [events,setEvents] = useState("")
+
 useEffect(() => {
-    fetchEvents()
-    return
-  },[]);
-const fetchEvents = () => {
     axios.get("https://eventeasynew.azurewebsites.net/api/Event/GetAll")
 	.then(response => {
-        console.log(response.data)
         setEvents(response.data)
+        console.log(events)
     })
-}
-const getEveList = () =>{
-    console.log(events)
-}
+    
+  },[]);
+
     return (
         <div>
             <Header/>
             <div className="events">
                 <ul className="eve-col">
-                    <li className="eve-row">
-                    {/* {
-                        events? 
+                    {/* <li className="eve-row">
+                    {
+                        events.list? 
                         events.map((ele,index)=>{
                         return <div key={index} className="eve-ele">
-                        {ele.eventName}
+                        {
+                            <Link className="getService" to='/result'>
+                                <h3>{ele.eventName}</h3>
+                            </Link>
+                        
+                        }
                         </div>
                         })
-                        :''
-                    } */}
-                    </li>
+                        :
+                        ''
+                    } 
+                    </li> */}
                     <li className="eve-row">
                         <div className="eve-ele">
-                            default mode
+                        <Link className="getService" to='/result'>
+                                <h3>Default Events</h3>
+                        </Link>
                         </div>
                         <div className="eve-ele">
-                            default mode
+                        <Link className="getService" to='/result'>
+                                <h3>Default Events</h3>
+                        </Link>
                         </div>
                         <div className="eve-ele">
-                            default mode
+                        <Link className="getService" to='/result'>
+                                <h3>Default Events</h3>
+                        </Link>
                         </div>
                         <div className="eve-ele">
-                            default mode
+                        <Link className="getService" to='/result'>
+                                <h3>Default Events</h3>
+                        </Link>
                         </div>
                     </li>
                 </ul>
-                <button><Link className="getService" to='/result' 
-                style={{color:'#ffffff'}}
-                
-                >Get Services</Link></button>
                 {/* <button onClick={getEveList}>Test</button> */}
             </div>
-          
+            <Footer/>
         </div>
     )
 
