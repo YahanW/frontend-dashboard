81ff947c0915a6438a66ab9e4f55121e53099976
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-08-23 12:53:46 +1000

    event go to services

--- src/layout/Header/Trolley.jsx
+++ src/layout/Header/Trolley.jsx
@@ -13,12 +13,15 @@ export default function Trolley(){
             console.log("Make request")
         }
     }
-    const events = [{
-    "eventName":"Wedding Ceremony",
-    "token":"E1U12-23aug-1",
-    "merchant":"Stark",
-    "accept":false,
-},]
+    const events = [
+        {"eventName":"Wedding Ceremony",
+        "token":"E1U12-23aug-1",
+        "merchant":"Stark",
+        "accept":false,},
+        {"eventName":"Corporate Party",
+        "token":"E2U12-23aug-2",
+        "merchant":"Elon",
+        "accept":true,},]
     return (
         <div>
              <a  onClick={showModalCheck} className="tro-item">Trolley</a>
@@ -30,8 +33,10 @@ export default function Trolley(){
                  {
                     events.map((ele,index)=>{
                         return (
-                        <li onClick={()=>setServiceVisible(true)}>
-                        <div className='avatar'></div>
+                        <li>
+                        <div className='avatar' onClick={()=>setServiceVisible(true)}>
+                            Services
+                        </div>
                         <div className='left'>
                             <h3>{ele.eventName}</h3>
                             <h5>{ele.merchant}</h5>
@@ -39,7 +44,7 @@ export default function Trolley(){
                             {ele.accept?'Accepted':'Waiting'}
                               </h4>
                           </div>
-                          <div className='right'>
+                          <div className='right' >
                           <button onClick={()=>sentRequest(ele.accept)}>
                             {
                               ele.accept?(
