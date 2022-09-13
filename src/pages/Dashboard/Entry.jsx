787675162a65ea484f77ab9bd31e966d2ccdf14b
YahanW <yahanw@utas.edu.au>
2022-09-13 22:08:33 +1000

    random css

--- src/pages/Dashboard/Entry.jsx
+++ src/pages/Dashboard/Entry.jsx
@@ -4,8 +4,13 @@ import Slider from '../../layout/slider';
 import Header from '../../layout/Header';
 import './dashboard.css';
 import Navbar from '../Home/homes/Navbar';
+import { ThreeDots } from 'react-loader-spinner';
+
 export default class Entry extends Component {
+
+
   render() {
+
     return (
       <div className='dashboard'> 
         <Navbar/>
@@ -13,6 +18,7 @@ export default class Entry extends Component {
           <div className='m-slide'>
             <Slider/>
           </div>
+
           <div className='m-content'>  
             {/**dynamic page */}
             <Outlet/>
