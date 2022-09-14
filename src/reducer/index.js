c5d4d3dfc76155e86ffc20dfeec3d4f0362e60e5
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-09-14 17:47:00 +1000

    reset

--- src/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import service from "./service";
-import user from "./user";
-import event from "./event";
-export default combineReducers({
-    service,
-    user,
-    event
-})
