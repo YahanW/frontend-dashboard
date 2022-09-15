a8428cc665ec7f77918856c9e5ddd536c96fd020
YahanW <yahanw@utas.edu.au>
2022-09-15 22:11:20 +1000

    change checkout

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -6,21 +6,34 @@ import Header from "../../layout/Header";
 import Footer from '../Home/homes/Footer';
 import Navbar from "../Home/homes/Navbar";
 import axios from "axios";
+import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
 
 
 function CheckOut() {
     const { eventId } = useParams();
     const [eventService, setEventService] = useState([]);
+    const [event, setEvent] = useState([]);
+    const eventType = ["Wedding","Birthday", "Business Function"];
+    var price = 0;
 
-    const getServices = async () => {
-        const { data } = await
-            axios.get(`https://eventeasyau.azurewebsites.net/api/eventservice/getservicesbyevent/${eventId}`);
-        console.log(data)
-        return data.$values
+    const getServices = () => {
+            axios.get(`https://eventeasyau.azurewebsites.net/api/eventservice/getservicesbyevent/${eventId}`)
+                .then(response => {
+                    setEventService(response.data.$values)
+                    console.log(response.data.$values)
+                }).catch(err => { console.log(err) })
+    }
+    const getEvent = () => {
+        axios.get(`https://eventeasyau.azurewebsites.net/api/event/get/${eventId}`)
+            .then(response => {
+                setEvent(response.data)
+                console.log(response.data)
+            }).catch(err => { console.log(err) })
     }
 
     useEffect(() => {
-        getServices().then((eventService) => setEventService(eventService))
+        getServices()
+        getEvent()
     }, []);
 
     return (
@@ -30,9 +43,9 @@ function CheckOut() {
                 <div className="itemContainer">
                     <h1 style={{ padding: '1rem' }}>CHECK OUT</h1>
 
-                    <h2 style={{ marginLeft: '1rem' }}>EVENT NAME: Corporate Feast</h2>
-                    <h4 style={{ marginLeft: '1rem' }}>event type: Corporate function</h4>
-                    <h2 style={{ marginLeft: '1rem' }}>DATE: 19/08/2022</h2>
+                    <h2 style={{ marginLeft: '1rem' }}>EVENT NAME: {event.eventName}</h2>
+                    <h2 style={{ marginLeft: '1rem' }}>EVENT TYPE: {eventType[event.eventType]}</h2>
+                    <h4 style={{ marginLeft: '1rem' }}>DATE: {event.startTime} - {event.endTime}</h4>
 
                     <div className="serList">
 
@@ -45,10 +58,10 @@ function CheckOut() {
                                         <div className="left">
                                             <h2 className="sname">Service Name {ele.services.serviceName}</h2>
                                             <div className="detail">
-                                                <h3>Order Period: {ele.services.startTime}-{ele.services.endTime} </h3>
                                                 <h3>  Quantity: 1</h3>
-                                                <h3>Price: {ele.services.price}</h3>
-                                                <button>remove</button>
+                                                <h3>Price: {ele.services.price!=null?ele.services.price:"N/A"}</h3>
+                                                {price += ele.services.price}
+                                                {/* <button>remove</button> */}
                                             </div>
                                         </div>
 
@@ -57,6 +70,9 @@ function CheckOut() {
                             })
                         }
 
+                    </div>
+                    <div className="totalPrice">
+                        <h2>Total: ${price} USD</h2>
                     </div>
                 </div>
                 <div className="paypalContainer">
